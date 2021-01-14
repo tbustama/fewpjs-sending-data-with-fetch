1192322dfcb005d5e03976f3acda8daacf0218fb
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-14 11:06:59 -0800

    commit tree 71f083c4a7114ee46d47a382abbc526146ef33bc

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const newP = document.createElement("p");
+      newP.textContent = object.id;
+      document.body.appendChild(newP);
+      return object;
+    })
+    .catch(function (error) {
+      let errorP = document.createElement("p");
+      errorP.textContent = error.message;
+      document.body.appendChild(errorP);
+    });
+}
